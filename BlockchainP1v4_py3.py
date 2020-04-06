b240f1b166dbd33ce12d6b779661ee80ce245531
Codi West <codiwest257@gmail.com>
2020-04-06 15:42:30 -0500

    Documentation and Results

--- BlockchainP1v4_py3.py
+++ BlockchainP1v4_py3.py
@@ -1,3 +1,14 @@
+'''
+Finds BTC addresses with shared spending and outputs balances of all shared addresses
+
+Uses API from Blockchain.info
+
+Set global settings near the top of the file
+Set address list to check at the bottom of the file
+
+Arghya Mukherjee
+Codi West
+'''
 
 import time  # use time.sleep(number of seconds) to wait between requests
 import requests
@@ -18,11 +29,16 @@ cached_results = True
 # Continues past single depth of linked addresses
 recursive = True
 # Stop recursive searching at given depth (only if recursive is enabled)
-depth_limit = 5
+depth_limit = None
 
 
 def get_raw_address(address, cached=cached_results):
-    ''' Return JSON response of raw address. Optionally uses cache.'''
+    '''
+    Return JSON response of raw address
+
+    Saves JSON response to disk 
+    Optionally uses cached saved response if possible
+    '''
 
     local_filename = "test_data/" + address + ".json"
     # Does not query website if cached in local filesystem
@@ -50,7 +66,11 @@ def get_raw_address(address, cached=cached_results):
 
 
 def find_shared_spending(address):
-    ''' Identifies and returns set of addresses which share this address as a transaction input '''
+    '''
+    Identifies and returns set of addresses which share this address as a transaction input
+
+    Input addresses differing from the input['prev_out']['addr'] format are ignored
+    '''
     linked_addresses = set()
 
     json_response = get_raw_address(address)
@@ -82,49 +102,84 @@ def find_shared_spending(address):
 
 
 def get_final_balances(address_set, force_local=cached_results):
-    ''' Returns JSON of final balance queries '''
+    '''
+    Returns JSON of final balance queries
+
+    Will attempt to get balances from local files if force_local = True
+    When querying blockchain.info, 130 addresses per query seems to be the limit
+    '''
     baseurl = 'https://blockchain.info/balance?active='
 
-    query = "|".join(address_set)
-
     # This will sum the final balances from local cached results
-    if force_local or len(address_set) > 20:
+    if force_local:
         json_response = {}
         if debug:
             print("Summing balances from local cache..")
         for address in address_set:
+            incomplete = False
             local_filename = "test_data/" + address + ".json"
             if path.exists(local_filename):
                 with open(local_filename) as file:
                     address_response = json.load(file)
                 json_response[address] = {'final_balance': address_response['final_balance']}
-    # Queries blockchain.info, but didn't work for 600+ addresses
+            else:
+                incomplete = True
+        if incomplete:
+            print("Warning: Incomplete balance due to some addresses not being cached on filesystem. Try online search?")
+    # Queries blockchain.info directly for balances
     else:
         if debug:
             print("Querying balances..")
-        time.sleep(sleep_time)
-        r = requests.get(baseurl+query, headers=headers)
-        json_response = json.loads(r.text)
+        # Does not work for 130+ addresses with single query, so may need to split
+        if len(address_set) < 130:
+            query = "|".join(address_set)
+            time.sleep(sleep_time)
+            r = requests.get(baseurl+query, headers=headers)
+            json_response = json.loads(r.text)
+        else:
+            address_list = list(address_set)
+            chunks = [address_list[x:x+130] for x in range(0, len(address_list), 130)]
+            if debug:
+                print("Estimated time: " + str(len(chunks) * sleep_time / 60) + " Minutes")
+            json_response = {}
+            for chunk in chunks:
+                query = "|".join(chunk)
+
+                time.sleep(sleep_time)
+                r = requests.get(baseurl+query, headers=headers)
+                partial_response = json.loads(r.text)
+                try: # Merge dictionaries - Python 3.5+
+                    json_response = {**json_response, **partial_response}
+                except: # Using older Python like a pleb
+                    print("Warning: Incomplete balance due to old Python version")
+                    return json_response
 
     return json_response
 
 
 def print_balances(balance_json, print_all=debug):
+    ''' Prints balance information passed from the get_final_balances function'''
     total = []
     for key, value in balance_json.items():
-        if print_all:
+        if print_all and len(balance_json.items()) < 50:
             print(str(key) + "\t " + str(value['final_balance']))
         total.append(int(value['final_balance']))
-    print("Balance for all linked address:\t\t", sum(total) / 100000000, " BTC (", sum(total), "Satoshis)")
+    print("Balance for all linked address:\t\t", sum(total) / 100000000, "BTC (", sum(total), "Satoshis)")
 
 
 def ensure_dir(file_path):
+    ''' Ensures that a directory exists on the local filesystem '''
     directory = path.dirname(file_path)
     if not path.exists(directory):
         makedirs(directory)
 
 
 def main(address, recursive=recursive, depth_limit=depth_limit):
+    '''
+    Finds addresses with shared spending to a given address
+
+    May limit the depth of searching 
+    '''
     checked_addresses = set()
     to_check = set()
 
@@ -154,12 +209,10 @@ def main(address, recursive=recursive, depth_limit=depth_limit):
 
             to_check = linked_addresses - checked_addresses
             depth += 1
-            # to_check.pop()
 
     print("Results for address:\t\t\t", address)
     print("Total linked addresses:\t\t\t", len(linked_addresses))
     print("Depth:\t\t\t\t\t", depth)
-
 
     balances_json = get_final_balances(linked_addresses)
 
@@ -169,10 +222,10 @@ def main(address, recursive=recursive, depth_limit=depth_limit):
 if __name__=="__main__":
 
     address_list=[
-        '15hzoz4YzGnj9Pqu8eNmhw4tBpDGjNgwj9',
-        # '36bt43aDDvsMJRd48YiRh6LLrNm3qnGZW5',
-        # '19ere2oJzJh81A5Q64SExDZYz54RvWHqZz',
-        # '19QDGMRKdZ9BpDZP2Re6yaDqNQ7zN4wo1D'
+        # '15hzoz4YzGnj9Pqu8eNmhw4tBpDGjNgwj9',
+        '36bt43aDDvsMJRd48YiRh6LLrNm3qnGZW5',
+        '19ere2oJzJh81A5Q64SExDZYz54RvWHqZz',
+        '19QDGMRKdZ9BpDZP2Re6yaDqNQ7zN4wo1D'
         ]
 
     for address in address_list:
---
+++ Readme.md
@@ -0,0 +1,36 @@
+# Shared Spending Address Finder
+
+This program finds BTC addresses with shared spending and outputs balances of all shared addresses
+
+Uses API from Blockchain.info
+
+Created by: Arghya Mukherjee and Codi West
+
+## Running the program with Python 3
+
+Use the `BlockchainP1_py3.py` file
+
+Run with:
+
+```bash
+python BlockchainP1_py3.py
+```
+
+Settings:
+
+* Set global settings near the top of the file
+* Set BTC address list to check at the bottom of the file
+
+## Running the program with Python 2
+
+Use the `BlockchainP1_py3.py` file
+
+Run with:
+
+```bash
+python BlockchainP1v4.py
+```
+
+Settings:
+
+* Set BTC address list to check at the top of the file
